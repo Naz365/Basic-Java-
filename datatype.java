e99170ca8036b5623a3fc7196151657cb8307d06
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-10-11 12:06:10 +0600

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,17 @@
+public class datatype {
+    public static void main(String[] args) {
+        
+        int num = 10;
+        char name ='N';
+        float f = 10.4f;
+        double d = 3.341;
+        short s = 145;
+        boolean B = true;
+        System.out.println("Number = "+num);
+        System.out.println("Char = "+name);
+        System.out.println("Float = "+f);
+        System.out.println("Double = "+d);
+        System.out.println("Short = "+s);
+        System.out.println("Boolean = "+B);
+    }
+}
